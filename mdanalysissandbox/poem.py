6600e486ebd42cf744fe242a228c826fbf6a7b28
ali <ali.ehlen@gmail.com>
2018-11-13 14:13:08 -0600

    important contribution

---
+++ mdanalysissandbox/poem.py
@@ -0,0 +1,8 @@
+""" here is a docstring """
+
+def write_a_poem():
+    print("We must, in our research and courses\n
+           Track atom positions and forces\n
+           To make this robust\n
+           Let's use code that we trust\n
+           That's versioned, free, and open source(s)")
