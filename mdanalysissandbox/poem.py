679c2e3557970220e71af008f396f3bd4c8eef91
ali <ali.ehlen@gmail.com>
2018-11-13 14:36:54 -0600

    addres PR #6 comments

--- mdanalysissandbox/poem.py
+++ mdanalysissandbox/poem.py
@@ -1,8 +1,9 @@
 """ here is a docstring """
 
 def write_a_poem():
-    print("We must, in our research and courses\n
+    """ return a poem """
+    return "We must, in our research and courses\n
            Track atom positions and forces\n
            To make this robust\n
            Let's use code that we trust\n
-           That's versioned, free, and open source(s)")
+           That's versioned, free, and open source(s)"
